71e09dc22a005492ee23c1debe32ba30ca735824
Peter Conley <peterdavidconley@gmail.com>
2022-02-09 18:04:16 -0500

    wrote PUT endpoint

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -39,11 +39,12 @@ router.post('/', validateUser, (req, res, next) => {
 
 });
 
-router.put('/:id', validateUserId, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-
+router.put('/:id', validateUserId, (req, res, next) => {
+  User.update(req.params.id, { name: req.name })
+  .then(updatedUser => {
+    res.json(updatedUser)
+  })
+  .catch(next)
 });
 
 router.delete('/:id', validateUserId, validateUser, (req, res) => {
