155ae804140ecb22f1e57f8432eb32d65a3070ac
Peter Conley <peterdavidconley@gmail.com>
2022-02-09 16:38:49 -0500

    wrote ValidateUserId middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,6 +1,7 @@
 const User = require('../users/users-model')
 
 function logger(req, res, next) {
+
   const timestamp = new Date().toLocaleString();
   const method = req.method
   const url = req.url
@@ -9,12 +10,24 @@ function logger(req, res, next) {
 
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
 
-  let { id } = req.params
-  console.log(`validateUserID`)
-  next()
-
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    } else {
+      req.user = user
+      res.status(201).json(user)
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error fetching user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
